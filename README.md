b38bdc3f3a492fdb1ff609e2b50e86a9cc70a8fc
jesus <chucho.cisneros@hotmail.com>
2021-08-25 21:05:25 -0500

    colegio
    pagina web del colegio de ccyt

---
+++ README.md
@@ -0,0 +1,2 @@
+# pagina del colegio de ccyt
+trabajo relisado en el servicio social
---
+++ colegio_CYT/js/button2.js
@@ -0,0 +1,26 @@
+/*****************************
+UACM| Colegio de ciencia y tecnología
+botones Cuerpo colegiados
+******************************/
+
+$(document).ready(function () {
+     $("#boton1").click(function () {
+         $("#panel1").slideToggle("slow");
+     });
+     $("#boton2").click(function () {
+         $("#panel2").slideToggle("slow");
+     });
+     $("#boton3").click(function () {
+         $("#panel3").slideToggle("slow");
+     });
+     $("#boton4").click(function () {
+         $("#panel4").slideToggle("slow");
+     });
+     $("#boton5").click(function () {
+         $("#panel5").slideToggle("slow");
+     });
+
+     $("#boton6").click(function () {
+         $("#panel6").slideToggle("slow");
+     });
+ });
