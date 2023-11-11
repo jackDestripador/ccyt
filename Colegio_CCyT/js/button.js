72eb017ec857fae93ca20b8577231edae61025df
Jesús Cisneros <chucho.cisneros@hotmail.com>
2023-11-11 16:10:34 -0600

    actualizar

--- Colegio_CCyT/js/button.js
+++ Colegio_CCyT/js/button.js
@@ -4,23 +4,26 @@ botones Cuerpo colegiados
 ******************************/
 
 $(document).ready(function () {
-     $("#boton1").click(function () {
-         $("#panel1").slideToggle("slow");
-     });
-     $("#boton2").click(function () {
-         $("#panel2").slideToggle("slow");
-     });
-     $("#boton3").click(function () {
-         $("#panel3").slideToggle("slow");
-     });
-     $("#boton4").click(function () {
-         $("#panel4").slideToggle("slow");
-     });
-     $("#boton5").click(function () {
-         $("#panel5").slideToggle("slow");
-     });
+  $("#boton1").click(function () {
+    $("#panel1").slideToggle("slow");
+  });
+  $("#boton2").click(function () {
+    $("#panel2").slideToggle("slow");
+  });
+  $("#boton3").click(function () {
+    $("#panel3").slideToggle("slow");
+  });
+  $("#boton4").click(function () {
+    $("#panel4").slideToggle("slow");
+  });
+  $("#boton5").click(function () {
+    $("#panel5").slideToggle("slow");
+  });
 
-     $("#boton6").click(function () {
-         $("#panel6").slideToggle("slow");
-     });
- });
+  $("#boton6").click(function () {
+    $("#panel6").slideToggle("slow");
+  });
+  $("#boton7").click(function () {
+    $("#panel7").slideToggle("slow");
+  });
+});
